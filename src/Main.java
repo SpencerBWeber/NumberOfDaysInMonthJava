39e8ca514587baece2a05a38f1208420b0fb5c64
JingleDjango95 <spencerweber019@gmail.com>
2019-10-23 17:59:09 -0600

    finalized solution

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,6 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(NumberOfDaysInMonth.getDaysInMonth(2, 2020));
+        System.out.println(NumberOfDaysInMonth.getDaysInMonth(1, -1));
+    }
 }
